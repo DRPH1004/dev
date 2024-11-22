e650a8ae7968660cb4530bf13c0eebbb3a2f4e29
DRPH1004 <jperez78@ucol.mx>
2024-11-21 20:21:55 -0600

    Inicio del objeto math

---
+++ javascript/junior/13 Objeto Math.js
@@ -0,0 +1,22 @@
+
+// _______________ Metodos _______________
+
+//sqrt() devuelve la raiz cuadrada de un numero
+// let resultado = Math.sqrt(9);
+
+//cbrt() devuelve la raiz cubica de un numero
+// let resultado = Math.cbrt(9);
+
+// max() devuelve el numero mas grande de los que le demos
+// let resultado=Math.max(1,52,85,74,62,5,48,52,0,8)
+
+// max() devuelve el numero mas pequeño de los que le demos
+// let resultado=Math.min(1,52,85,74,62,5,48,52,0,8);
+
+// devuelve un numero aleatorio entre 0 y 1
+// let resultado=Math.random();
+
+
+
+
+document.write(resultado);
